9730a4b312f1fc3195e172617098b9affec54165
HeoMin <hurmin13@gmail.com>
2023-03-30 11:47:26 +0900

    c

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
