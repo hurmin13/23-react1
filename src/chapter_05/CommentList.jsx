2e882a6812ac45159c9a1050156d1f84c85598d2
HeoMin <hurmin13@gmail.com>
2023-04-06 11:12:18 +0900

    ㅊ

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -3,16 +3,16 @@ import Comment from "./Comment";
 
 const comments = [
     {
-        name: "이인제",
-        comment: "안녕하세요, 소플입니다.",
+        name: "허민",
+        comment: "안녕하세요, 허민입니다.",
     },
     {
-        name: "유재석",
-        comment: "리액트 재미있어요~!",
+        name: "허민1",
+        comment: "안녕하세요, 허민1입니다.",
     },
     {
-        name: "강민경",
-        comment: "저도 리액트 배워보고 싶어요!!",
+        name: "허민2",
+        comment: "안녕하세요, 허민2입니다.",
     },
 ];
 
